25ee4e877f183a2bf9d80949b107367d4d20e47d
“AnuragParashar2000” <anuragparashar03@gmail.com>
2023-09-28 04:59:17 +0530

    removed comments

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Your Unsplash API access key
 const accessKey = 'input_your_api_key';
 
-// Number of images to fetch
 const numberOfImages = 10;
 
-// Reference to the gallery container
 const gallery = document.getElementById('art-gallery');
 
-// Fetch images from Unsplash API
 fetch(`https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${numberOfImages}`)
    .then((response) => response.json())
    .then((data) => {
